4f2f981899f8ca84587656cf5f7264e293c392a3
Sameera Leola <sameeraleola@gmail.com>
2020-05-08 08:19:01 -0700

    Starting with ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,14 +1,13 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 class Ticket:
     def __init__(self, source, destination):
-        self.source = source
-        self.destination = destination
+        # This is the linked list
+        self.source = source # current
+        self.destination = destination # pointer to next
+       
 
 
 def reconstruct_trip(tickets, length):
-    
-    """
-    YOUR CODE HERE
-    """
+    hashtable = {}
 
     return route
