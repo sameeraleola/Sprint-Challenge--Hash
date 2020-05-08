a15ce1a861fc04ad7acd71fabf6f02f392972866
Sameera Leola <sameeraleola@gmail.com>
2020-05-08 11:19:01 -0700

    My repo was broken

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,19 @@
 def has_negatives(a):
+    # populate the nums hashtable with all of the positive values
+    # iterate a for negative values
+    # check num for a positive
+    # populate the negs array if it is found.
+    nums = dict()
+    negs = []
 
-    """
-    YOUR CODE HERE
-    """
+    # Put the positives in the nums dictionary
+    for num in a:
+        if num > 0:
+           nums[num] = num
+         
+
+
+    
 
     return result
 
