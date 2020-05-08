e53165fa2454320f06c84761eb8c3497dadb96e0
Sameera Leola <sameeraleola@gmail.com>
2020-05-08 08:50:46 -0700

    Must add Ken to repo

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,10 +4,20 @@ class Ticket:
         # This is the linked list
         self.source = source # current
         self.destination = destination # pointer to next
-       
 
-
+"""       
+Understanding: Since this can run in linear time a for loop to find the head can be done.
+Plan:
+1. Define the hash table to store the reconstructed trip (sould be named route since that's what's returned)
+Loop the tickets to find the head and add it to the hash table
+"""
 def reconstruct_trip(tickets, length):
+    # Store the
     hashtable = {}
+    # The reconstructed trip is stored here.
+    route = [None] * length
+
+    # Look for the head
+    for 
 
     return route
